e96f944637c63d9532c556952e54152eead3fab3
yunwuxin <448901948@qq.com>
2020-03-11 00:41:07 +0800

    自动生成注释

--- src/Service.php
+++ src/Service.php
@@ -3,15 +3,20 @@
 namespace think\workflow;
 
 use Doctrine\Common\Annotations\Reader;
+use ReflectionClass;
 use Symfony\Component\Workflow\DefinitionBuilder;
 use Symfony\Component\Workflow\Registry;
+use Symfony\Component\Workflow\SupportStrategy\InstanceOfSupportStrategy;
 use Symfony\Component\Workflow\Transition;
 use Symfony\Component\Workflow\Workflow;
+use think\ide\ModelGenerator;
 use think\Model;
 use think\workflow\annotation\StateMachine;
 
 class Service extends \think\Service
 {
+    protected $detect = [];
+
     public function boot(Reader $reader)
     {
         /** @var Registry $registry */
@@ -22,26 +27,45 @@ class Service extends \think\Service
             /** @var Reader $reader */
             $reader = $this->app->make(Reader::class);
 
-            Model::maker(function (Model $model) use ($reader) {
+            Model::maker(function (Model $model) use ($registry, $reader) {
 
-                $annotations = $reader->getClassAnnotations(new \ReflectionClass($model));
+                $className = get_class($model);
+                if (!isset($this->detect[$className])) {
+                    $annotations = $reader->getClassAnnotations(new ReflectionClass($model));
+
+                    foreach ($annotations as $annotation) {
+                        if ($annotation instanceof StateMachine) {
+                            $builder = new DefinitionBuilder();
+                            $builder->addPlaces($annotation->places);
+                            foreach ($annotation->transitions as $transition) {
+                                $builder->addTransition(new Transition($transition->value, $transition->from, $transition->to));
+                            }
+                            $builder->setInitialPlaces($annotation->initial);
+                            $definition   = $builder->build();
+                            $marking      = new ModelMarkingStore($annotation->value);
+                            $stateMachine = new Workflow($definition, $marking, null, get_class($model) . "@" . $annotation->value);
+
+                            $registry->addWorkflow($stateMachine, new InstanceOfSupportStrategy(get_class($model)));
+
+                            foreach ($annotation->transitions as $transition) {
+                                call_user_func([$model, 'macro'], $transition->value, function ($context = []) use ($transition, $stateMachine) {
+                                    $stateMachine->apply($this, $transition->value, $context);
+                                });
+                            }
+                        }
+                    }
+                    $this->detect[$className] = true;
+                }
+            });
+
+            $this->app->event->listen(ModelGenerator::class, function (ModelGenerator $generator) use ($reader) {
+
+                $annotations = $reader->getClassAnnotations($generator->getReflection());
 
                 foreach ($annotations as $annotation) {
                     if ($annotation instanceof StateMachine) {
-                        $builder = new DefinitionBuilder();
-                        $builder->addPlaces($annotation->places);
                         foreach ($annotation->transitions as $transition) {
-                            $builder->addTransition(new Transition($transition->value, $transition->from, $transition->to));
-                        }
-                        $builder->setInitialPlaces($annotation->initial);
-                        $definition   = $builder->build();
-                        $marking      = new ModelMarkingStore($annotation->value);
-                        $stateMachine = new Workflow($definition, $marking);
-
-                        foreach ($annotation->transitions as $transition) {
-                            call_user_func([$model, 'macro'], $transition->value, function ($context = []) use ($transition, $stateMachine) {
-                                $stateMachine->apply($this, $transition->value, $context);
-                            });
+                            $generator->addMethod($transition->value, 'void', ['$context = []'], '');
                         }
                     }
                 }
